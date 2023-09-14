228c05d693025bf712b6307c8495079d8b00e763
Nicolas Janvier <janvier.nico@gmail.com>
2023-09-14 17:29:53 +0200

    feat: add save/load as file

--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -1,5 +1,7 @@
 import * as React from "react";
-// import { useStorage } from "./Storage";
+import { usePictures } from "./PicturesContext";
+import { useFrames } from "./FramesContext";
+import logo from "/gallery/src/assets/logo.svg";
 
 type Props = {
   onResumeClick: () => void;
@@ -7,36 +9,68 @@ type Props = {
 };
 
 export const Header: React.FC<Props> = ({ onResumeClick, showResume }) => {
-  //   const [save, load] = useStorage();
+  const btnClasses =
+    "box-content rounded border-[1px] border-solid border-transparent bg-transparent px-4 py-2 transition-all hover:border-indigo-600";
 
-  //   const [name, setName] = React.useState("");
+  const { pictures, loadPictures } = usePictures();
+  const { frames, loadFrames } = useFrames();
 
-  //   const handleSave = () => {
-  //     if (!name) {
-  //       return;
-  //     }
-  //     save(name);
-  //   };
+  const [loading, setLoading] = React.useState(false);
+  const onSave = () => {
+    setLoading(true);
+    const json = { pictures, frames };
+    const jsonString = `data:text/json;chatset=utf-8,${encodeURIComponent(
+      JSON.stringify(json)
+    )}`;
+    const link = document.createElement("a");
+    link.href = jsonString;
+    link.download = "gallery.json";
 
-  //   const loadSave = () => {
-  //     const name = load();
-  //     if (name) {
-  //       setName(name);
-  //     }
-  //   };
+    link.click();
+    setLoading(false);
+  };
 
-  //   const btnClasses =
-  //     "box-content rounded border-[1px] border-solid border-transparent bg-transparent px-4 py-2 transition-all hover:border-indigo-600";
+  const onImportSave: React.ChangeEventHandler<HTMLInputElement> = (e) => {
+    setLoading(true);
+    const fileReader = new FileReader();
+    if (!e.target.files?.[0]) return;
+    fileReader.readAsText(e.target.files?.[0], "UTF-8");
+    fileReader.onload = (e) => {
+      if (e.target === null) return;
+      const data = JSON.parse(e.target.result as string);
+      if (data.pictures && Array.isArray(data.pictures)) {
+        loadPictures(data.pictures);
+      }
+      if (data.frames) {
+        loadFrames(data.frames);
+      }
+      setLoading(false);
+    };
+  };
 
+  const inputRef = React.useRef<HTMLInputElement>(null);
   return (
     <header className="flex h-16 items-center gap-4 border-b-2 border-solid border-indigo-600 bg-white px-4 py-1 text-indigo-600">
-      <p className="mr-auto font-serif text-2xl tracking-wider">Gallery</p>
-      {/* <button className={`ml-auto ${btnClasses}`} onClick={handleSave}>
+      <p className="mr-auto flex items-center gap-2 font-serif text-2xl tracking-wider">
+        <img src={logo} className="inline-block h-6 w-6" />
+        Gallery
+      </p>
+      <button
+        className={btnClasses}
+        onClick={() => inputRef.current?.click()}
+        disabled={loading}
+      >
+        Load
+      </button>
+      <input
+        type="file"
+        onChange={onImportSave}
+        ref={inputRef}
+        className="hidden"
+      />
+      <button className={btnClasses} onClick={onSave} disabled={loading}>
         Save
       </button>
-      <button className={btnClasses} onClick={loadSave}>
-        Load
-      </button> */}
       <button
         className={`box-content rounded bg-indigo-600 px-4 py-2 text-white hover:bg-indigo-900 disabled:bg-indigo-300`}
         onClick={onResumeClick}
