65bb3ab886b99a51febc4e57af383109ad9da79f
Nicolas Janvier <janvier.nico@gmail.com>
2023-09-15 12:20:30 +0200

    fix: cleanup header

--- src/App.tsx
+++ src/App.tsx
@@ -1,36 +1,21 @@
 import * as React from "react";
-import { FramesProvider, useFrames } from "./components/FramesContext";
+import { FramesProvider } from "./components/FramesContext";
 import { Template } from "./components/Template";
-import Modal from "./components/Modal";
-import { FramesListSummary } from "./components/FramesListSummary";
 import { PictureLibrary } from "./components/PictureLibrary";
 import { PicturesProvider } from "./components/PicturesContext";
 import { PictureModal } from "./components/PictureModal";
 import { Header } from "./components/Header";
 
 export const App = function () {
-  const { frames } = useFrames();
-  const [showResumeModal, setShowResumeModal] = React.useState(false);
   const [selectedId, setSelectedId] = React.useState<string>();
-  const showSummaryButton = Object.values(frames).some((f) => f.pictureId);
 
   return (
     <div className="relative h-screen">
-      <Header
-        showResume={showSummaryButton}
-        onResumeClick={() => setShowResumeModal(true)}
-      />
+      <Header />
       <main className="flex h-[calc(100vh-4rem)] w-screen flex-auto items-center justify-center bg-gray-200">
         <Template onSelect={setSelectedId} />
         <PictureLibrary onSelect={setSelectedId} />
       </main>
-      <Modal
-        open={showResumeModal}
-        onClose={() => setShowResumeModal(false)}
-        title="Summary"
-      >
-        <FramesListSummary />
-      </Modal>
       <PictureModal id={selectedId} onClose={() => setSelectedId("")} />
     </div>
   );
--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -2,18 +2,19 @@ import * as React from "react";
 import { usePictures } from "./PicturesContext";
 import { useFrames } from "./FramesContext";
 import logo from "/src/assets/logo.svg";
+import Modal from "./Modal";
+import { FramesListSummary } from "./FramesListSummary";
 
-type Props = {
-  onResumeClick: () => void;
-  showResume: boolean;
-};
+type Props = { foo?: string };
 
-export const Header: React.FC<Props> = ({ onResumeClick, showResume }) => {
+export const Header: React.FC<Props> = () => {
+  const [showResumeModal, setShowResumeModal] = React.useState(false);
   const btnClasses =
-    "box-content rounded border-[1px] border-solid border-transparent bg-transparent px-4 py-2 transition-all hover:border-indigo-600";
+    "box-content rounded border-[1px] border-solid border-transparent bg-transparent px-4 py-2 transition-all enabled:hover:border-indigo-600";
 
   const { pictures, loadPictures } = usePictures();
   const { frames, loadFrames } = useFrames();
+  const hasFrames = Object.values(frames).some((f) => f.pictureId);
 
   const [loading, setLoading] = React.useState(false);
   const onSave = () => {
@@ -50,34 +51,47 @@ export const Header: React.FC<Props> = ({ onResumeClick, showResume }) => {
 
   const inputRef = React.useRef<HTMLInputElement>(null);
   return (
-    <header className="flex h-16 items-center gap-4 border-b-2 border-solid border-indigo-600 bg-white px-4 py-1 text-indigo-600">
-      <p className="mr-auto flex items-center gap-2 font-serif text-2xl tracking-wider">
-        <img src={logo} className="inline-block h-6 w-6" />
-        Gallery
-      </p>
-      <button
-        className={btnClasses}
-        onClick={() => inputRef.current?.click()}
-        disabled={loading}
+    <>
+      <header className="flex h-16 items-center gap-4 border-b-2 border-solid border-indigo-600 bg-white px-4 py-1 text-indigo-600">
+        <p className="mr-auto flex items-center gap-2 font-serif text-2xl tracking-wider">
+          <img src={logo} className="inline-block h-6 w-6" />
+          Gallery
+        </p>
+        <button
+          className={btnClasses}
+          onClick={() => inputRef.current?.click()}
+          disabled={loading}
+        >
+          Load
+        </button>
+        <input
+          type="file"
+          onChange={onImportSave}
+          ref={inputRef}
+          className="hidden"
+        />
+        <button
+          className={btnClasses}
+          onClick={onSave}
+          disabled={loading || !hasFrames}
+        >
+          Save
+        </button>
+        <button
+          className={`box-content rounded bg-indigo-600 px-4 py-2 text-white hover:bg-indigo-900 disabled:bg-indigo-300`}
+          onClick={() => setShowResumeModal(true)}
+          disabled={!hasFrames}
+        >
+          Summary
+        </button>
+      </header>
+      <Modal
+        open={showResumeModal}
+        onClose={() => setShowResumeModal(false)}
+        title="Summary"
       >
-        Load
-      </button>
-      <input
-        type="file"
-        onChange={onImportSave}
-        ref={inputRef}
-        className="hidden"
-      />
-      <button className={btnClasses} onClick={onSave} disabled={loading}>
-        Save
-      </button>
-      <button
-        className={`box-content rounded bg-indigo-600 px-4 py-2 text-white hover:bg-indigo-900 disabled:bg-indigo-300`}
-        onClick={onResumeClick}
-        disabled={!showResume}
-      >
-        Summary
-      </button>
-    </header>
+        <FramesListSummary />
+      </Modal>
+    </>
   );
 };
