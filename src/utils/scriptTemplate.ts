38ae1986c52297b8ace351d7cf5232122f36f71a
Nicolas Janvier <nj@kanpla.dk>
2025-05-03 11:13:01 +0200

    style: improve code formatting and error handling in components

--- src/utils/scriptTemplate.ts
+++
@@ -1,8 +0,0 @@
-function generateScript(fileNames: string[]): string {
-  let script = `mkdir -p export;`;
-  for (const fileName of fileNames) {
-    script += ` find . -type f -name "${fileName}" -exec cp {} export/ \\;`;
-  }
-  script += ` echo "Files copied to export"`;
-  return script;
-}
